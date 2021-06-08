3b85f9386266e05e24776f9e8dd21854e38dd02d
Mateus Riva <mateus.riva@telecom-paristech.fr>
2021-06-08 14:52:58 +0200

    Run extra task and extra output

--- run.py
+++ run.py
@@ -11,20 +11,21 @@ import torch
 import tqdm
 from itertools import product
 
-from data import FetaDataset
-from unet import UNet3D, UNet3D_extratask
-from train import train_model_base, train_model_extrainput, train_model_extratask, train_model_extraoutput
+from data import FetaDataset2D as FetaDataset
+#from unet import UNet3D, UNet3D_extratask, UNet3D_extraoutput
+from unet import UNet, UNet_extratask, UNet_extraoutput
+from train import train_model_base, train_model_extrainput, train_model_extraoutput
 
 if __name__ == '__main__':
     # Experimental attributes and parameters:
     # Batch size
-    batch_size=1
+    batch_size=8
     # Learning rate
     lr=0.001
     # Device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     # configuration label
-    experimental_configs = ["base", "extra_input","extra_task","extra_output","extra_step"]
+    experimental_configs = ["extra_output"]#["base", "extra_input","extra_task","extra_output","extra_step"]
     # Number of repetitions per configuration (rep number is used as random seed)
     repetitions=1
 
@@ -37,24 +38,24 @@ if __name__ == '__main__':
         torch.manual_seed(seed); np.random.seed(seed); random.seed(seed)
 
         # Obtaining dataset
-        dataset = FetaDataset(data_path="/home/mriva/Recherche/feta_2.0")
-        train_set, val_set, test_set = torch.utils.data.random_split(dataset, (60, 10, 10))
+        dataset = FetaDataset(data_path="./data")
+        train_set, val_set, test_set = torch.utils.data.random_split(dataset, (int(0.8*dataset.size), int(0.1*dataset.size), int(0.1*dataset.size)))
         data_loaders = {"train": torch.utils.data.DataLoader(train_set,batch_size=batch_size,num_workers=2),
                         "val": torch.utils.data.DataLoader(val_set,batch_size=batch_size,num_workers=2),
                         "test": torch.utils.data.DataLoader(test_set,batch_size=batch_size,num_workers=2)}
 
         # Setting up model
         if experimental_config == "base": # Traditional, baseline UNet, random init
-            model = UNet3D(input_channels=1, output_channels=8)
+            model = UNet(input_channels=1, output_channels=8)
             model = model.to(device)
         elif experimental_config == "extra_input":
-            model = UNet3D(input_channels=2, output_channels=8)
+            model = UNet(input_channels=2, output_channels=8)
             model = model.to(device)
         elif experimental_config == "extra_task":
-            model = UNet3D_extratask(input_channels=1, output_channels=8)
+            model = UNet_extratask(input_channels=1, output_channels=8)
             model = model.to(device)
         elif experimental_config == "extra_output":
-            model = UNet3D(input_channels=1, output_channels=9)
+            model = UNet_extraoutput(input_channels=1, output_channels=8)
             model = model.to(device)
         else:
             continue
@@ -74,12 +75,12 @@ if __name__ == '__main__':
 
         # Train network
         if experimental_config == "base":
-            model, train_history = train_model_base(model, optimizer, loss_functions, data_loaders, device,n_class=8)
+            model, train_history = train_model_base(model, optimizer, loss_functions, data_loaders, device,n_class=8, max_epochs=2)
         if experimental_config == "extra_input":
-            model, train_history = train_model_extrainput(model, optimizer, loss_functions, data_loaders, device,n_class=8)
+            model, train_history = train_model_extrainput(model, optimizer, loss_functions, data_loaders, device,n_class=8, max_epochs=2)
         if experimental_config == "extra_task":
-            model, train_history = train_model_extratask(model, optimizer, loss_functions, data_loaders, device,n_class=8)
+            model, train_history = train_model_extraoutput(model, optimizer, loss_functions, data_loaders, device,n_class=8, max_epochs=2)
         if experimental_config == "extra_output":
-            model, train_history = train_model_extraoutput(model, optimizer, loss_functions, data_loaders, device,n_class=9)
+            model, train_history = train_model_extraoutput(model, optimizer, loss_functions, data_loaders, device,n_class=8, max_epochs=2)
         else:
             continue
--- unet.py
+++ unet.py
@@ -126,28 +126,29 @@ class UNet3D(nn.Module):
 
 
 
-class UNet3D_extratask(nn.Module):
+class UNet_extratask(nn.Module):
     def __init__(self, input_channels, output_channels):
         super().__init__()
 
-        self.dconv_down1 = double_conv3d(input_channels, 64)
-        self.dconv_down2 = double_conv3d(64, 128)
-        self.dconv_down3 = double_conv3d(128, 256)
-        self.dconv_down4 = double_conv3d(256, 512)
+        self.dconv_down1 = double_conv(input_channels, 64)
+        self.dconv_down2 = double_conv(64, 128)
+        self.dconv_down3 = double_conv(128, 256)
+        self.dconv_down4 = double_conv(256, 512)
 
-        self.maxpool = nn.MaxPool3d(2)
-        self.upsample = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=True)
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
 
-        self.galinear1 = nn.Linear(256,256)
+        self.gaconv = double_conv(512, 32)
+        self.galinear1 = nn.Linear(32*64*64,64)
         self.gaactivation1 = nn.ReLU(inplace=True)
-        self.galinear2 = nn.Linear(256,1)
+        self.galinear2 = nn.Linear(64,1)
         self.gaactivation2 = nn.ReLU(inplace=True)
 
-        self.dconv_up3 = double_conv3d(256 + 512, 256)
-        self.dconv_up2 = double_conv3d(128 + 256, 128)
-        self.dconv_up1 = double_conv3d(128 + 64, 64)
+        self.dconv_up3 = double_conv(256 + 512, 256)
+        self.dconv_up2 = double_conv(128 + 256, 128)
+        self.dconv_up1 = double_conv(128 + 64, 64)
 
-        self.conv_last = nn.Conv3d(64, output_channels, 1)
+        self.conv_last = nn.Conv2d(64, output_channels, 1)
 
 
     def forward(self, x):
@@ -178,7 +179,69 @@ class UNet3D_extratask(nn.Module):
 
         out1 = self.conv_last(x)
 
-        x2 = self.galinear1(x2)
+        x2 = self.gaconv(x2)
+        x2 = self.galinear1(x2.reshape(x2.size(0),-1))
+        x2 = self.gaactivation1(x2)
+        out2 = self.galinear2(x2)
+        #out2 = self.gaactivation2(x2)
+
+        return out1, out2
+
+class UNet_extraoutput(nn.Module):
+    def __init__(self, input_channels, output_channels):
+        super().__init__()
+
+        self.dconv_down1 = double_conv(input_channels, 64)
+        self.dconv_down2 = double_conv(64, 128)
+        self.dconv_down3 = double_conv(128, 256)
+        self.dconv_down4 = double_conv(256, 512)
+
+        self.maxpool = nn.MaxPool2d(2)
+        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+        self.dconv_up3 = double_conv(256 + 512, 256)
+        self.dconv_up2 = double_conv(128 + 256, 128)
+        self.dconv_up1 = double_conv(128 + 64, 64)
+
+        self.gaconv = double_conv(128+64, 16)
+        self.galinear1 = nn.Linear(16*256*256,64)
+        self.gaactivation1 = nn.ReLU(inplace=True)
+        self.galinear2 = nn.Linear(64,1)
+        self.gaactivation2 = nn.ReLU(inplace=True)
+
+        self.conv_last = nn.Conv2d(64, output_channels, 1)
+
+
+    def forward(self, x):
+        conv1 = self.dconv_down1(x)
+        x = self.maxpool(conv1)
+
+        conv2 = self.dconv_down2(x)
+        x = self.maxpool(conv2)
+
+        conv3 = self.dconv_down3(x)
+        x = self.maxpool(conv3)
+
+        x = self.dconv_down4(x)
+
+        x = self.upsample(x)
+
+        x = torch.cat([x, conv3], dim=1)
+
+        x = self.dconv_up3(x)
+        x = self.upsample(x)
+        x = torch.cat([x, conv2], dim=1)
+
+        x = self.dconv_up2(x)
+        x = self.upsample(x)
+        x2 = torch.cat([x, conv1], dim=1)
+
+        x = self.dconv_up1(x2)
+
+        out1 = self.conv_last(x)
+
+        x2 = self.gaconv(x2)
+        x2 = self.galinear1(x2.reshape(x2.size(0),-1))
         x2 = self.gaactivation1(x2)
         out2 = self.galinear2(x2)
         #out2 = self.gaactivation2(x2)
